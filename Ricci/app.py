088907fc7c60cc6c9605f792c47b1a1215b0acd7
Woongjin Yoo <Woongjin Yoo>
2017-02-01 15:26:15 +0900

    Ricci

--- Ricci/app.py
+++ Ricci/app.py
@@ -1,1 +1,16 @@
-//python app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/hi")
+def hi():
+    return 'hi!'
+
+
+#main
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
