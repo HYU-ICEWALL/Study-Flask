82aca2ec1d700f7b9e6f06706f910b445a9e7c36
MaybeS <maytryark@gmail.com>
2017-02-01 11:14:16 +0900

    Update request logout

--- 1. Response/app.py
+++ 1. Response/app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return """
+<div class="col-md-6 col-md-offset-3">
+  <form action="/logout" method="POST">
+    <button type="submit" class="btn btn-default">Logout</button>
+  </form>
+</div>
+        """
+    else:
+        return render_template('home.html')
 
 @app.route("/login", methods=['POST'])
 def login():
-    print (request.form)
+    session['username'] = request.form['email']
     return redirect('/')
 
+@app.route("/logout", methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return redirect('/')
 
 # main
 if __name__ == "__main__":
+    app.secret_key='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(host='0.0.0.0', debug=True)
