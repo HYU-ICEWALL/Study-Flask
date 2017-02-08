3134b6c4b1ad64a3721e3721e6a35d8e32099fa0
MaybeS <maytryark@gmail.com>
2017-02-08 10:24:32 +0900

    Update gitignore

--- 0. Prepare/app.py
+++ 0. Prepare/app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return """
+<form action="/logout" method="POST">
+  <button type="submit" class="btn btn-default">logout</button>
+</form>"""
+    else:
+        return render_template('home.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+    session['username'] = request.form['email']
+    return redirect(url_for('home'))
+
+@app.route("/logout", methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 # main
 if __name__ == "__main__":
+    app.secret_key = 'a'
     app.run(host='0.0.0.0', debug=True)
+
+
+
+
+
