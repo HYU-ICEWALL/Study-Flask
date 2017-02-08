5d241e6ce88665c307d1a35fe6dcc2dfd15ac210
MaybeS <maytryark@gmail.com>
2017-02-08 14:04:04 +0900

    Second Day

--- 1. Response/app.py
+++ 1. Response/app.py
@@ -25,6 +25,42 @@ def logout():
     session.pop('username', None)
     return redirect('/')
 
+import os
+import json
+@app.route("/read", methods=["GET"])
+def read():
+    arts = []
+    for file in os.listdir('data/'):
+        with open('data/' + file, 'r') as f:
+            arts.append(json.load(f))
+    return render_template('read.html', articles=arts)
+
+@app.route("/write", methods=["GET", "POST"])
+def write():
+    if request.method == 'GET':
+        return render_template('write.html')
+    elif request.method == 'POST':
+        dic = dicfy(request.form['title'], request.form['text'], now())
+        with open('data/' + hash(), 'w') as f:
+            json.dump(dic, f)
+        return redirect(url_for('read'))
+
+from random import randrange
+def hash(len=10):
+    return ''.join([ str(randrange(0, 9)) for _ in range(len) ])
+
+def dicfy(title, text, date):
+    d = {
+        'title': title,
+        'text': text,
+        'date': date,
+    }
+    return d
+
+from datetime import datetime
+def now():
+    return datetime.now().strftime('%Y-%m-%d')
+
 # main
 if __name__ == "__main__":
     app.secret_key='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
